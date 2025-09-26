7a82ca488d8c2b3c6abfe3cba9f3e471cc948cbe
Mohit Kumar Mourya <118112037+MohitMourya01@users.noreply.github.com>
2025-09-26 19:20:36 +0530

    Update AiModel.jsx
    model updated

--- src/service/AiModel.jsx
+++ src/service/AiModel.jsx
@@ -9,7 +9,7 @@ import { GoogleGenerativeAI } from "@google/generative-ai";
   const genAI = new GoogleGenerativeAI(apiKey);
   
   const model = genAI.getGenerativeModel({
-    model: "gemini-1.5-flash",
+    model: "gemini-2.5-flash",
     systemInstruction: "Generate Travel Plan for Location: Las Vegas, for 3 Days for Couple with a Cheap budget, Give me a Hotels options list with HotelName, Hotel address, Price, hotel image url, geo coordinates, rating, descriptions and suggest itinerary with placeName, Place Details, Place Image Url, Geo Coordinates, ticket Pricing, rating, Time travel each of the location for 3 days with each day plan with best time to visit in JSON format.",
   });
   
